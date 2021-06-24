23c2dbed2079cff8f34191ebdeb98c493008b2ba
Ryan Berwald <rberwald@gmail.com>
2021-06-24 00:42:13 -0400

    Moved various functions and variables to config class to  simplify code, added string interpolation for sending messages to server, configured keypresses for each key :

--- hotkey.py
+++ hotkey.py
@@ -1,44 +1,46 @@
-from typing import AnyStr
 import keyboard
 import websocket
-import toml
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler 
 import threading
-import logging
+
+from config_class import config
+
+
 
 class Event(LoggingEventHandler):
     def dispatch(self, event):
         if event.src_path == ".\config.toml" and event.event_type == "modified":
-            print(f'event type: {event.event_type}  path : {event.src_path}')
+            conf.loadConfig()
 
-def keyPress():
+
+
+def keyPress(profile):
+    print(f'hotkey pressed {profile}')
     ws = websocket.WebSocket()
     ws.connect("ws://localhost:6805/client")
-    ws.send("changeprofile=Desk")
+    ws.send(f'changeprofile={profile}')
     print(ws.recv())
     ws.close()
-
-def debugPress():
-    print("pressed hotkey")
 
 def observe():
     event_handler = Event()
     observer = Observer()
     observer.schedule(event_handler, ".", recursive=True)
-    observer.start()
+    observer.start()      
 
 
-obsThread = threading.Thread(target=observe)
-
-obsThread.start()
+conf = config("./config.toml", keyPress)
 
 
-config = toml.load("./config.toml")
-keys = config["Hotkeys"]
+def main():
+    obsThread = threading.Thread(target=observe)
+    obsThread.start()
+    
+    print("Press ESC to stop.")
+    keyboard.wait('esc')
 
-for key in keys["keys"]:
-    keyboard.add_hotkey(key, debugPress) #<-- attach the function to hot-key
+if __name__ == "__main__":
+    main()
 
-print("Press ESC to stop.")
-keyboard.wait('esc')
+

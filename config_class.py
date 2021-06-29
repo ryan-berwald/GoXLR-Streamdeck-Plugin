1cb76fd84ee86fb94be4f9876de1c81e970a75b1
Ryan Berwald <rberwald@gmail.com>
2021-06-29 00:10:24 -0400

    Added exception logging

--- config_class.py
+++ config_class.py
@@ -1,22 +1,26 @@
+import logging
 import keyboard
 import toml
-import sys
+import logging
 
 class config:
-    def __init__(self, _path, _keyPressFunc):
+    def __init__(self, _keyPressFunc, _path):
         self.path = _path
         self.rawfile = []
         self.configFile = None
         self.profiles = []
         self.hotkeyFunc = _keyPressFunc
         self.keys = []
-        try:
-            self.loadConfig()
-        except:
-            print("Unexpected error:", sys.exc_info()[0])
-            raise
+        self.loadConfig()
+        logging.getLogger().info(f"\n\tPath: {self.path}\n\tProfiles: {self.profiles}\n\tKeys: {self.keys}")
+
     def loadConfig(self):
-        self.configFile = toml.load(self.path)
+        try:
+            self.configFile = toml.load(self.path)
+        except FileNotFoundError as e:
+            logging.getLogger().error(e)
+            raise e
+            
         self.rawfile = self.configFile["Hotkeys"]
         print(self.rawfile)
         self.keys = self.rawfile["keys"]

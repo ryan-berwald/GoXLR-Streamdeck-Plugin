93fe8ffd40515d09cb9821a61f14490c35b2b7c8
Ryan Berwald <rberwald@gmail.com>
2021-08-25 00:40:01 -0400

    Added small user interface

--- ui.py
+++ ui.py
@@ -1,25 +1,38 @@
 from PyQt5.QtGui import * 
 from PyQt5.QtWidgets import * 
-  
-  
-app = QApplication([])
-app.setQuitOnLastWindowClosed(False)
-  
-# Adding an icon
-icon = QIcon("./Assets/icon.ico")
-  
-# Adding item on the menu bar
-tray = QSystemTrayIcon()
-tray.setIcon(icon)
-tray.setVisible(True)
-  
-# Creating the options
-menu = QMenu()
-option1 = QAction("Geeks for Geeks")
-option2 = QAction("GFG")
-menu.addAction(option1)
-menu.addAction(option2)
-  
+
+class userInterface:
+    def __init__():
+        app = QApplication([])
+        app.setQuitOnLastWindowClosed(False)
+        
+        # Adding an icon
+        icon = QIcon("./Assets/icon.ico")
+        
+        # Adding item on the menu bar
+        tray = QSystemTrayIcon()
+        tray.setIcon(icon)
+        tray.setVisible(True)
+        
+        # Creating the options
+        menu = QMenu()
+        option1 = QAction("Reload Config")
+        menu.addAction(option1)
+        menu.addAction(option2)
+
+        window = QWidget()
+        layout = QVBoxLayout()
+
+        WsCB = QCheckBox("WebSocket Running")
+        WsCB.setEnabled(False)
+
+        GoXLRCB = QCheckBox("GoXLR Connection")
+        GoXLRCB.setEnabled(False)
+
+        layout.addWidget(QPushButton('Edit Config'))
+        window.setLayout(layout)
+        window.show()
+
 # To quit the app
 quit = QAction("Quit")
 quit.triggered.connect(app.quit)

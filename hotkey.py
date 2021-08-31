0980683dd14ecfe50a8627a0517eadc2ea08a73c
Ryan Berwald <rberwald@gmail.com>
2021-08-30 21:48:24 -0400

    Started cleaning up random code blocks

--- hotkey.py
+++ hotkey.py
@@ -1,6 +1,5 @@
 import websocket
-from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler 
+
 import threading
 import logging
 from subprocess import Popen, PIPE
@@ -9,7 +8,7 @@ from sys import exit as ex
 from os import mkdir, path
 from pathlib import Path
 from ui import userInterface
-from shutil import which
+from Server import Server
 
 goXlrDir = str(Path.home()) + '\\Documents\\GoXLRPlugin\\'
 #Setup logger with format 
@@ -31,6 +30,7 @@ def keyPress(profile, keys):
         ws = websocket.WebSocket()
         try:
             ws.connect("ws://localhost:6805/client")
+
         except Exception as e:
             logger.error(e)
         logger.info(f'Sending profile change request {profile}')
@@ -40,65 +40,18 @@ def keyPress(profile, keys):
         logger.error(e)
     finally:
         ws.close()
-
-try:
-    conf = config(keyPress, goXlrDir + 'config.toml')
-except FileNotFoundError:
-    with open(goXlrDir + 'config.toml', 'w') as file:
-        file.write("""# Define keys to be pressed in parallel array with profiles.
-# ex. keys=["F1", "F2"]
-#     profiles=["profile1", "profile2"]\n
-# This will set profile1 when you press F1 and set profile2 when press F2
-
-title = "GoXLR Streamdeck Emulator"\n
-
-[Server]
-GoXLRAddress="ws://localhost:6805/?GoXLRApp"
-ClientAddress="ws://localhost:6805/client="
-
-[Hotkeys]
-keys=["F13", "CTRL + B"]
-profiles=["Desk", "Game"] 
-""")
-    conf = config(keyPress, goXlrDir + 'config.toml')
-
-class Event(LoggingEventHandler):
-    def dispatch(self, event):
-        if event.src_path == goXlrDir + "config.toml" and event.event_type == "modified":
-                conf.loadConfig()
-
-def verifyConnection(systray):
-    try:
-        websocket.WebSocket().connect("ws://localhost:6805/client")    
-    except ConnectionRefusedError as e:
-        logger.error(e)
-
-def startServer():
-    try:
-        process = Popen([which("node"), 'C:\\Users\\Ryan\\Documents\\GoXLR-Streamdeck-Plugin\\goxlr.js'])
-        if process.poll() == None:
-            print("started")
-    except Exception as e:
-        print(e)
-    
-def observe():
-    event_handler = Event()
-    observer = Observer()
-    observer.schedule(event_handler, ".", recursive=True)
-    observer.start()     
     
 def main():
     try:
-        #TO DO -- Implement UI
-        obsThread = threading.Thread(target=observe, daemon=True)
-        obsThread.start()
+        """ obsThread = threading.Thread(target=observe, daemon=True)
+        obsThread.start() """
         #Try to start websocket server
-        print("starting server...")
-        """ nodeThread = threading.Thread(target=startServer, daemon=True)
-        nodeThread.start() """
-        startServer()
+        ########server#######
+        server = Server()
+        server.startServer()
         logger.info("Listening for hotkeys...")
-        print("Press ESC to stop.")
+        connThread = threading.Thread(target=verifyConnection, daemon=True)
+        connThread.start()
         ui = userInterface(goXlrDir)
     except KeyboardInterrupt as e:
         logger.error(e)       

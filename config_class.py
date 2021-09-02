d7aa0ee7d391a41a0702ac05beb8247ad36087c3
Ryan Berwald <rberwald@gmail.com>
2021-09-02 18:27:51 -0400

    Added server passthrough to keypress event

--- config_class.py
+++ config_class.py
@@ -6,13 +6,14 @@ from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler 
 
 class config:
-    def __init__(self, keyPressFunc, path, goxlrdir):
+    def __init__(self, keyPressFunc, path, goxlrdir, server):
         self.path = path
         self.rawfile = []
         self.configFile = None
         self.profiles = []
         self.hotkeyFunc = keyPressFunc
         self.keys = []
+        self.server = server
         self.loadConfig(goxlrdir)
         logging.getLogger().info(f"\n\tPath: {self.path}\n\tProfiles: {self.profiles}\n\tKeys: {self.keys}")
 
@@ -20,7 +21,7 @@ class config:
         try:
             self.configFile = toml.load(self.path)
         except FileNotFoundError as e:
-            logging.getLogger().error(e)
+            logging.getLogger().error("Config file not found, creating sample now")
             with open(goXlrDir + 'config.toml', 'w') as file:
                 file.write("""# Define keys to be pressed in parallel array with profiles.
 # ex. keys=["F1", "F2"]
@@ -45,11 +46,11 @@ profiles=["Desk", "Game"]
             print(self.keys)
             print(self.profiles)
             for x in range(len(self.keys)):
-                add_hotkey(self.keys[x], self.hotkeyFunc, args=(self.profiles[x], self.keys[x])) #<-- attach the function to hot-key    
+                add_hotkey(self.keys[x], self.hotkeyFunc, args=(self.profiles[x], self.keys[x], self.server)) #<-- attach the function to hot-key    
 
-    def observe(self):
+"""     def observe(self):
         event_handler = FileObserver.FileModified()
         observer = Observer()
         observer.schedule(event_handler, ".", recursive=True)
-        observer.start()  
+        observer.start()   """
 

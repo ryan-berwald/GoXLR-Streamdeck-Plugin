496beb8c1a809a3d741fbd34e6b252b1dc667b05
Ryan Berwald <rberwald@gmail.com>
2021-09-04 01:33:05 -0400

    Added exedir and began implementing start with windows functionality

--- uiTKinter.py
+++ uiTKinter.py
@@ -1,9 +1,10 @@
 # Import the required libraries
 import tkinter as tk
+from tkinter import filedialog
 from pystray import MenuItem as item
 import pystray
 from PIL import Image, ImageTk
-from os import system
+from os import system, getcwd
 
 
 class ui:
@@ -11,7 +12,7 @@ class ui:
     CROSSMARK = "\u274c"
     def __init__(self, goXlrDir) -> None:
         self.goXlrDir = goXlrDir
-
+        self.exeDir = ""
         # Create an instance of tkinter frame or self.window
         self.win=tk.Tk()
 
@@ -33,7 +34,9 @@ class ui:
         #Creating frame and packing buttons
         self.frm_Buttons = tk.Frame()
         self.btn_EditConfig = tk.Button(text="Edit Config", master=self.frm_Buttons, command=lambda: system(goXlrDir + "config.toml"))
+        self.chk_StartWindows = tk.Checkbutton(text="Start with Windows", master=self.frm_Buttons, command=self.start_with_windows )
         self.btn_EditConfig.pack()
+        self.chk_StartWindows.pack()
         self.frm_Buttons.pack()
         self.win.title("Streamdeck GoXLR Emulator")
 
@@ -61,4 +64,25 @@ class ui:
         icon=pystray.Icon("name", image, "My System Tray Icon", menu)
         icon.run()
 
+    def start_with_windows(self):
+        while True: 
+            goXLRExe = filedialog.askopenfile(initialdir=getcwd())
+            
+
+
+            putItHere = str("")
+
+
+
+
+
+            print(goXLRExe)
+            if goXLRExe.name.split("/")[len(goXLRExe.name.split("/"))-1] == "GoXLR App.exe":
+                import winreg;
+                key = winreg.OpenKey(winreg.HKEY_CURRENT_USER,
+                r'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run', 0,
+                winreg.KEY_SET_VALUE); winreg.SetValueEx(key, 'GoXLRPlugin', 0,
+                winreg.REG_SZ, f'{goXLRExe.name}'); # file_path is path of file after coping it
+
+                break
     
